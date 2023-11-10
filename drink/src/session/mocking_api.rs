bd4a61296ae2971d77c928b61a34fd8490aa270d
pgherveou <pgherveou@gmail.com>
2023-11-10 10:57:54 +0100

    Updates from previous PR

--- drink/src/session/mocking_api.rs
+++ drink/src/session/mocking_api.rs
@@ -1,24 +1,42 @@
 //! Mocking API for the sandbox.
+use super::Session;
 use crate::{
     mock::ContractMock,
     runtime::{AccountIdFor, RuntimeWithContracts},
-    Sandbox, DEFAULT_GAS_LIMIT,
+    DEFAULT_GAS_LIMIT,
 };
 
-impl<R: RuntimeWithContracts> Sandbox<R> {
+pub struct MockingApi<'a, R: RuntimeWithContracts> {
+    session: &'a mut Session<R>,
+}
+
+impl<'a, R: RuntimeWithContracts> MockingApi<'a, R> {
+    /// Create a new MockingApi
+    pub fn new(session: &'a mut Session<R>) -> Self {
+        MockingApi { session }
+    }
+
     /// Deploy `mock` as a standard contract. Returns the address of the deployed contract.
     pub fn deploy(&mut self, mock: ContractMock) -> AccountIdFor<R> {
         // We have to deploy some contract. We use a dummy contract for that. Thanks to that, we
         // ensure that the pallet will treat our mock just as a regular contract, until we actually
         // call it.
         let mock_bytes = wat::parse_str(DUMMY_CONTRACT).expect("Dummy contract should be valid");
+        let salt = self
+            .session
+            .mocks
+            .lock()
+            .expect("Should be able to acquire lock on registry")
+            .salt();
+
         let mock_address = self
+            .session
+            .sandbox()
             .deploy_contract(
                 mock_bytes,
                 0u32.into(),
                 vec![],
-                // We have to use a different account ID for each contract.
-                vec![self.mock_counter as u8],
+                salt,
                 R::default_actor(),
                 DEFAULT_GAS_LIMIT,
                 None,
@@ -27,8 +45,8 @@ impl<R: RuntimeWithContracts> Sandbox<R> {
             .expect("Deployment of a dummy contract should succeed")
             .account_id;
 
-        self.mock_counter += 1;
-        self.mock_registry
+        self.session
+            .mocks
             .lock()
             .expect("Should be able to acquire lock on registry")
             .register(mock_address.clone(), mock);
